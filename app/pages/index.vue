75b7699fa35be1482616ee2be4e7cfcfa1252b33
tonnyvecher <sidoroffds@gmail.com>
2026-02-17 12:50:17 +0400

    SN-1|feat: реализовал основную первую страницу

---
+++ app/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="home">
+    <h1 class="home__title">тестовое задание</h1>
+
+    <NuxtLink class="btn btn--primary" to="/catalog">В каталог</NuxtLink>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/tokens' as t;
+@use '../assets/scss/mixins' as m;
+
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  margin: 0 auto;
+  min-height: 100vh;
+  padding-inline: 60px;
+  gap: 40px;
+
+  @include m.mq-down(xs) {
+    padding-inline: 20px;
+  }
+
+  &__title {
+    font-weight: 500;
+    font-size: 64px;
+    line-height: 110%;
+    letter-spacing: 0%;
+    text-transform: uppercase;
+
+    @include m.mq-down(xs) {
+      font-size: 26px;
+    }
+  }
+}
+</style>
